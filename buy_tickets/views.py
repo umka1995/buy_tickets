e85bd0183383d7fc4b2db06ed86c141ec5a782b0
Emka06 <kadyrbekovemir435@gmail.com>
2023-05-19 20:49:05 +0600

    cache2

--- buy_tickets/views.py
+++ buy_tickets/views.py
@@ -8,7 +8,7 @@ from .permissions import IsOwnerPermission, IsAdminOrActivePermission
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import filters
-from django.core.cache import cache
+
 
 
 
